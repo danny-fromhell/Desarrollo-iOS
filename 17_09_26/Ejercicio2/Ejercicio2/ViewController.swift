96630e7a147e2fee9de61923d34096e4057a6490
Daniel Lopez <chasingbandmembers@gmail.com>
2026-09-22 14:30:13 -0600

    Agregar Ejercicio2 y su evidencia

---
+++ 17_09_26/Ejercicio2/Ejercicio2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Ejercicio2
+//
+//  Created by Daniel López Barrón on 22/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+
+    
+    @IBOutlet weak var nameLabel: UILabel!
+
+    
+    @IBAction func greetButtonTapped(_ sender: Any) {
+        nameLabel.text = nameTextField.text
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
