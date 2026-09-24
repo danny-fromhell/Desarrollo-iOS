d8bcdf360d6fd306fadd6843c1ce3e7abca37598
Daniel Lopez <chasingbandmembers@gmail.com>
2026-09-24 14:12:49 -0600

    Agregar ejercicios con while del 24-09-2026

--- 24_09_26/Ejercicio1.playground/Contents.swift
+++ 24_09_26/Ejercicio1.playground/Contents.swift
@@ -1,8 +1,16 @@
 import UIKit
+import Foundation
 
 //Recorrer los números del 1 al 100
 for number in 1...100 {
     print(number)
+}
+//Con while
+var number = 1
+
+while number <= 100 {
+    print(number)
+    number += 1
 }
 
 //Recorrer el alfabeto y mostrar el índice
@@ -10,6 +18,17 @@ let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 for (index, letter) in alphabet.enumerated() {
     print("\(index): \(letter)")
+}
+//Con while
+var index = 0
+
+while index < alphabet.count {
+    let stringIndex = alphabet.index(alphabet.startIndex, offsetBy: index)
+    let letter = alphabet[stringIndex]
+    
+    print("\(index): \(letter)")
+    
+    index += 1
 }
 
 //Diccionario de estados y capitales y su recorrido
@@ -21,4 +40,15 @@ let statesAndCapitals: [String: String] = [
 for (state, capital) in statesAndCapitals {
     print("The capital of \(state) is \(capital).")
 }
+//Con while
+let states = Array(statesAndCapitals)
+var stateIndex = 0
 
+while stateIndex < states.count {
+    let state = states[stateIndex].key
+    let capital = states[stateIndex].value
+    
+    print("The capital of \(state) is \(capital).")
+    
+    stateIndex += 1
+}
